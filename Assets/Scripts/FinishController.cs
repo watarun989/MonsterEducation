ab9024ccbbf9d46821c57773f62e43c1804b99e6
Akio Mizukami <>
2025-03-29 10:47:44 +0400

    0329 FinishController Homework Completed

---
+++ Assets/Scripts/FinishController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class FinishController : MonoBehaviour
+{
+    public GameObject finishPanel; 
+    public GameObject confirmPanel; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        confirmPanel.SetActive(false); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onFinish(){
+        GameController.gameStatus = "FinishSelected"; 
+        confirmPanel.SetActive(true); 
+        finishPanel.SetActive(false); 
+    }
+
+    public void onFinishYes(){
+        Debug.Log("Finished"); 
+        StartCoroutine(onFinishYesCoroutine()); 
+        ReloadScene(); 
+    }
+
+    public void onFinishNo(){
+        finishPanel.SetActive(true); 
+        confirmPanel.SetActive(false); 
+        GameController.gameStatus = "wait"; 
+    }
+
+    public void ReloadScene(){
+        GameController.timePoint = 100; 
+        SceneManager.LoadScene("Main"); 
+    }
+
+    IEnumerator onFinishYesCoroutine(){
+        yield return new WaitForSeconds(5.0f); 
+    }
+}
