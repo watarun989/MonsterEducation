94fcf6fa9305495b81eb08ed541bd9f51b6ce775
Akio Mizukami <>
2025-05-19 17:36:43 +0400

    0519 Run Scene Completed

--- Assets/Scripts/FinishController.cs
+++ Assets/Scripts/FinishController.cs
@@ -2,22 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement; 
+using TMPro; 
 
 public class FinishController : MonoBehaviour
 {
     public GameObject finishPanel; 
     public GameObject confirmPanel; 
+    public TextMeshProUGUI daysText; 
+    static public int days = 1; 
 
     // Start is called before the first frame update
     void Start()
     {
+        daysText.text = "1"; 
         confirmPanel.SetActive(false); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        daysText.text = days.ToString(); 
     }
 
     public void onFinish(){
@@ -29,6 +33,25 @@ public class FinishController : MonoBehaviour
     public void onFinishYes(){
         Debug.Log("Finished"); 
         StartCoroutine(onFinishYesCoroutine()); 
+
+        // if(daysText == "1"){
+        //     daysText.text = "2"
+        // }else 
+        // if(daysText == "2"){
+        //     daysText.text = "3"
+        // }else 
+        // if(daysText == "3"){
+        //     daysText.text = "4"
+        // }else
+        // if(daysText == "4"){
+        //     daysText.text = "5"
+        // }else
+        // if(daysText == "5"){
+        //     daysText.text = "6"
+        // }else
+        // if(daysText == "6"){
+        //     daysText.text = "7"
+        // }
     }
 
     public void onFinishNo(){
--- Assets/Scripts/Run.cs
+++ Assets/Scripts/Run.cs
@@ -4,50 +4,58 @@ using UnityEngine;
 using TMPro; 
 using UnityEngine.SceneManagement; 
 
-public class Sleep : MonoBehaviour
+//SleepScene Run Script
+public class Run : MonoBehaviour
 {
+    float dir; 
+    public float speed = 3.0f; 
     public Camera cameraA; //Sleep Camera
     public Camera cameraB; //Main Camera
     public Camera cameraC; //Run Camera
-    public GameObject zzz; 
     public GameObject buttonPanel; 
     public int[,] statusPatterns = new int[,]{
-        {1,2,0},{0,1,-1},{0,-2,0}
+        {1,0,2},{1,0,-1},{-2,0,0}
     }; 
-    public TextMeshProUGUI sleepText; 
+    public TextMeshProUGUI runText; 
 
     // Start is called before the first frame update
     void Start()
     {
-
+        dir = 1; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        dir = Mathf.Sin(Time.time * 2); 
+
+        if(dir > 0){
+            transform.rotation = Quaternion.Euler(0,90,0); 
+        }else if(dir < 0){
+            transform.rotation = Quaternion.Euler(0,270,0); 
+        }
+
+        transform.position += transform.forward * speed * Time.deltaTime; 
     }
 
-    public void sleepSelected(){
-        cameraA.enabled = true; 
+    public void runSelected(){
+        cameraC.enabled = true; 
+        cameraA.enabled = false; 
         cameraB.enabled = false; 
-        cameraC.enabled = false; 
-        zzz.SetActive(true); 
         buttonPanel.SetActive(false); 
         int rand = Random.Range(0,3); 
         StatusController.attackPoint += statusPatterns[rand,0]; 
         StatusController.defencePoint += statusPatterns[rand,1]; 
         StatusController.speedPoint += statusPatterns[rand,2]; 
-        sleepText.text = "Your monster's attack power has changed by " + statusPatterns[rand,0] + " Point(s)! \n"; 
-        sleepText.text += "It's defence has changed by " + statusPatterns[rand,1] + " Point(s) too! \n"; 
-        sleepText.text += "Lastly, it's speed has changed by " + statusPatterns[rand,2] + " Point(s)! \n"; 
-        sleepText.text += "Click to go back. "; 
+        runText.text = "Your monster's attack power has changed by " + statusPatterns[rand,0] + " Point(s)! \n"; 
+        runText.text += "It's defence has changed by " + statusPatterns[rand,1] + " Point(s) too! \n"; 
+        runText.text += "Lastly, it's speed has changed by " + statusPatterns[rand,2] + " Point(s)! \n"; 
+        runText.text += "Click to go back. "; 
         StartCoroutine(WaitForClick()); 
     }
 
     IEnumerator WaitForClick(){
         yield return new WaitUntil(() => Input.GetMouseButtonDown(0)); 
-        FinishController.days ++; 
         SceneManager.LoadScene("Main"); 
     }
 }
